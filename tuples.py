d684e2f067b79ca3f02b70024666f72c4a5f1e47
Omar Hernandez <omrhvs@gmail.com>
2024-10-13 01:14:34 -0600

    tuples

---
+++ tuples.py
@@ -0,0 +1,12 @@
+# Creation & access
+punto = (3,4)
+
+print (punto[0]) # Prints 3
+print (punto[1]) # Prints 4
+
+# Methods
+mi_tupla = (1, 2, 3, 2, 4, 2)
+
+print (mi_tupla.index(2))    # Output: 1
+print (mi_tupla.index(2, 2))    # Output: 3
+print (mi_tupla.index(2, 2, 4))    # Output: 3
