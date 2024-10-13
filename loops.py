39bcc1fa0c5776ab6db172d73e8b00c0ca51a974
Omar Hernandez <omrhvs@gmail.com>
2024-10-13 00:28:37 -0600

    loops

---
+++ loops.py
@@ -0,0 +1,16 @@
+# For
+
+frutas = ["manzana", "banana", "naranja"]
+
+for fruta in frutas:
+    print (fruta)
+
+
+# While
+
+contador = 0
+
+while contador < 5:
+
+    print (contador)
+    contador += 1
