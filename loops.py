d7ebfdbb5e902dc29e9021eddafc276002fce61f
Omar Hernandez <omrhvs@gmail.com>
2024-10-13 00:43:20 -0600

    advanced control statements

--- loops.py
+++ loops.py
@@ -14,3 +14,25 @@ while contador < 5:
 
     print (contador)
     contador += 1
+
+# Advanced Control Statements - Break
+contador = 0
+
+while True:
+
+    print (contador)
+    contador += 1
+    
+    if contador == 5:
+            break
+
+# Continue
+for i in range(10):
+
+    if i % 2 == 0:
+            continue
+    print(i)
+
+# Pass
+for i in range(5):
+    pass
