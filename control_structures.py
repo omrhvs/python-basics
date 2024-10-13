5f10ee10f3e9764a628e701b6a9179269cef3ad7
Omar Hernandez <omrhvs@gmail.com>
2024-10-13 00:15:50 -0600

    control structures

---
+++ control_structures.py
@@ -0,0 +1,26 @@
+# Conditional Statements - IF
+edad = 18
+
+if edad >= 18:
+    print ("Eres mayor de edad.")
+
+# IF-ELSE
+if edad >= 18:
+    print ("Eres mayor de edad.")
+else:
+    print ("Eres menor de edad.")
+
+# IF-ELIF-ELSE
+calificacion = 85
+
+if calificacion >= 90:
+    print ("Excelente")
+
+elif calificacion >= 80:
+    print ("Muy bueno")
+
+elif calificacion >= 70:
+    print ("Bueno")
+
+else:
+    print ("Necesita mejorar")
--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,60 @@
-# Comentario
+# Comment
 
 """
-Comentario
-multi-linea
+multi-line
+comment
 """
-
 
 print("Hola Mundo!")
+
+edad = 25
+cantidad = 100
+
+precio = 9.99
+altura = 1.75
+
+nombre = "Juan"
+mensaje = 'Hola, Mundo!'
+
+es_mayor_de_edad = True
+tiene_descuento = False
+es_estudiante = True
+
+a = b = c = 10
+
+# Si
+Edad = 0
+nombre_completo = "Omar"
+total_ventas = 0
+_contador = 10
+
+# No
+# 1edad        # Number first
+# nombre-completo        # Dash instead of underscore
+# If        # Keyword
+
+
+a = 10
+b = 3
+
+# Aritmetic Operators
+suma = a + b        # 13
+resta = a - b        # 7
+multiplicacion = a * b        #30
+division = a / b        # 3.333333333
+division_entera = a // b        # 3
+modulo = a % b        # 1
+exponenciacion = a ** b        # 1000
+
+# Comparison Operators
+igual = a == b        # False
+diferente = a != b        # True
+mayor_que = a > b        # True
+menor_que = a < b        # False
+mayor_o_igual = a >= b        # True
+meno_o_igual = a <= b        # False
+
+# Logic
+resultado_and = (a > 5) and (b < 5) # True
+resultado_or = (a > 15) or (b < 5) # True
+resultado_not = not (a > 5) # False
