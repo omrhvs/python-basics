a6762f910b987c2dbf5cb2e07b54da26b62b8eaf
Omar Hernandez <omrhvs@gmail.com>
2024-10-13 01:53:07 -0600

    mini progress test

---
+++ test.py
@@ -0,0 +1,16 @@
+numero = 7
+
+if numero % 2 == 0:
+
+    resultado = "Par"
+    print(resultado)
+
+else:
+    resultado = "Impar"
+    print(resultado)
+
+def multiplicar (a, b):
+    return a * b
+
+resultado1 = multiplicar (5, 3) + multiplicar (2, 4)
+print(resultado1)
