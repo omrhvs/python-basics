1552f4bafa7c6d09e32022721ce764b887984d1c
Omar Hernandez <omrhvs@gmail.com>
2024-10-15 03:21:20 -0600

    modules

---
+++ modules.py
@@ -0,0 +1,16 @@
+# Import modules
+
+import math
+import random
+import datetime
+# from math import sqrt
+
+resultado = math.sqrt(25)
+# resultado = sqrt(25)
+print(resultado) # Prints 5.0
+
+numero_aleatorio = random.randint(1, 10)
+print(numero_aleatorio) # Prints a random number between 1 and 10
+
+fecha_actual = datetime.datetime.now()
+print(fecha_actual) # Prints current date and time
